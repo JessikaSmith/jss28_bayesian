f764621b1752a6d55a3f8e2c6bc180bc35595947
JessikaSmith <mariyaxod@yandex.ru>
2018-08-25 23:29:34 +0300

    Checkpoint

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
 __author__ = 'Maria Khodorchenko'
+import numpy as np
 
-def generate_data():
+
+def generate_normal_data(loc=0.0, scale=1.0, size=None):
+    return np.random.normal(loc=loc, scale=scale, size=None)
