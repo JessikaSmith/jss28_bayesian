5152b9765fe916d0b6b5e77c2cd50232c8817b88
maria <mariyaxod@yandex.ru>
2018-09-30 22:51:11 +0300

    Fix y definition

--- tests/test.py
+++ tests/test.py
@@ -5,7 +5,9 @@ from jss28_bayesian.visualization import *
 reg_model = RegressionModel(shape=(100, 30))
 print(reg_model.datax)
 var_selection_model = Model()
-trace_len = 200
+trace_len = 50000
 var_selection_model.createModel(reg_model.datax, reg_model.datay, trace_len=trace_len)
 #print(var_selection_model.trace)
-plot_params(trace_len, var_selection_model.trace[0][trace_len:, None], var_selection_model.trace[1][trace_len:, None])
+# print(var_selection_model.trace)
+#plot_params(trace_len, var_selection_model.trace['alpha'], var_selection_model.trace['e'][-1])
+#plot_trace(var_selection_model.trace['alpha'], var_selection_model.trace['theta'][-1])
